87d2d119a07802786f4a2591a39965a767f27c96
Caio Felipe Mendes de Sousa <csousa@daitan.com>
2020-11-26 17:16:07 -0300

    set up Routes, Login and custom Hook to fill up form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+
+import useFormValidation from './useFormValidation';
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+}
 
 function Login(props) {
-  return <div>Login</div>;
+  const {handleChange, handleSubmit, values}= useFormValidation(INITIAL_STATE);
+  const [login, setLogin] = useState(true);
+
+  return (
+    <div>
+      <h2 className='mv3'>{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className='flex flex-column'>
+        {!login && <input 
+          type='text'
+          placeholder='Your name'
+          autoComplete='off'
+          name='name'
+          value={values.name}
+          onChange={handleChange}
+        />}
+        <input 
+          type='email'
+          placeholder='Your email'
+          autoComplete='off'
+          name='email'
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input 
+          type='password'
+          placeholder='Choose a sacure password'
+          name='password'
+          value={values.password}
+          onChange={handleChange}
+        />
+        <div className='flex mt3'>
+          <button type='submit' className='button pointer mr2'>
+            Submit
+          </button>
+          <button type='button' className="pointer button" onClick={()=>setLogin(prevLogin => !prevLogin)}>
+            {login ? "need to create account" : "already have an account"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
